25606389f3ee520bcdf4daa2f3eb5d7abb77ede9
webkawsar <web.kawsarahmed@gmail.com>
2022-11-04 07:47:34 +0600

    fix login page when type input can't show input box for mobile keyboard

--- src/screens/SignIn.js
+++ src/screens/SignIn.js
@@ -4,6 +4,7 @@ import {
   ActivityIndicator,
   Image,
   Pressable,
+  ScrollView,
   StyleSheet,
   Text,
   View
@@ -22,86 +23,91 @@ const SignIn = ({ navigation }) => {
 
   const signIn = async () => {
     try {
-
-      const userCredential = await signInWithEmailAndPassword(auth, email, password);
+      const userCredential = await signInWithEmailAndPassword(
+        auth,
+        email,
+        password
+      );
       const user = userCredential.user;
       showMessage({
-        message: 'Sign in successfully',
-        type: 'success'
-      })
-      
+        message: "Sign in successfully",
+        type: "success",
+      });
     } catch (error) {
-      
-      console.log(error, 'signIn error');
+      console.log(error, "signIn error");
       showMessage({
-        message: 'Sign in failed',
-        type: 'danger'
-      })
+        message: "Sign in failed",
+        type: "danger",
+      });
     }
   };
 
   return (
     <SafeAreaView style={{ flex: 1 }}>
-      <View>
-        <Image
-          source={LoginImage}
-          style={{
-            width: 200,
-            height: 200,
-            alignSelf: "center",
-            marginVertical: 20,
-          }}
-        />
-        <Text style={{ fontSize: 18, fontWeight: "bold", textAlign: "center" }}>
-          Never forget your notes
-        </Text>
-      </View>
-
-      <View style={{ paddingHorizontal: 16, paddingVertical: 25 }}>
-        <Input
-          placeholder="Email"
-          autoCapitalize="none"
-          onChangeText={(text) => setEmail(text)}
-        />
-        <Input
-          placeholder="Password"
-          secureTextEntry
-          onChangeText={(text) => setPassword(text)}
-        />
-      </View>
-
-      <View
-        style={{
-          alignItems: "center",
-          flex: 1,
-          justifyContent: "flex-end",
-          paddingBottom: 40,
-        }}
-      >
-        {loading ? (
-          <ActivityIndicator />
-        ) : (
-          <Button
-            onPress={signIn}
-            title="Login"
-            customStyles={{ alignSelf: "center", marginBottom: 25 }}
+      <ScrollView>
+        <View>
+          <Image
+            source={LoginImage}
+            style={{
+              width: 200,
+              height: 200,
+              alignSelf: "center",
+              marginVertical: 20,
+            }}
           />
-        )}
-        <Pressable onPress={() => navigation.navigate("SignUp")}>
-          <Text>
-            Don't have an account?{" "}
-            <Text
-              style={{
-                fontWeight: "bold",
-                color: "green",
-                textDecorationLine: "underline",
-              }}
-            >
-              Sign up
-            </Text>
+          <Text
+            style={{ fontSize: 18, fontWeight: "bold", textAlign: "center" }}
+          >
+            Never forget your notes
           </Text>
-        </Pressable>
-      </View>
+        </View>
+
+        <View style={{ paddingHorizontal: 16, paddingVertical: 25 }}>
+          <Input
+            placeholder="Email"
+            autoCapitalize="none"
+            onChangeText={(text) => setEmail(text)}
+          />
+          <Input
+            placeholder="Password"
+            secureTextEntry
+            onChangeText={(text) => setPassword(text)}
+          />
+        </View>
+
+        <View
+          style={{
+            alignItems: "center",
+            flex: 1,
+            justifyContent: "flex-end",
+            paddingBottom: 40,
+          }}
+        >
+          {loading ? (
+            <ActivityIndicator />
+          ) : (
+            <Button
+              onPress={signIn}
+              title="Login"
+              customStyles={{ alignSelf: "center", marginBottom: 25 }}
+            />
+          )}
+          <Pressable onPress={() => navigation.navigate("SignUp")}>
+            <Text>
+              Don't have an account?{" "}
+              <Text
+                style={{
+                  fontWeight: "bold",
+                  color: "green",
+                  textDecorationLine: "underline",
+                }}
+              >
+                Sign up
+              </Text>
+            </Text>
+          </Pressable>
+        </View>
+      </ScrollView>
     </SafeAreaView>
   );
 };
